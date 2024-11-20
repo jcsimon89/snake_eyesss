54684aaca9553605a48ab0259615468c2a5158a1
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-11-20 12:23:07 -0800

    Update snake_bash.bat

--- workflow/snake_bash.bat
+++ workflow/snake_bash.bat
@@ -1,8 +1,8 @@
 @ECHO OFF
 ECHO Hello world
-cd C:\Users\jcsimon\Documents\GitHub\snake_brainsss\workflow
-call activate snake_brainsss
-snakemake --config user=jcsimon --profile profiles/config_Mac -s build_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\brainsss_imports\20240531
-snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\brainsss\JS68_x_JS117\fly_005
+cd C:\Users\jcsimon\Documents\GitHub\snake_eyesss\workflow
+call activate snake_eyesss
+snakemake --config user=jcsimon --profile profiles/config_Mac -s build_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\imports\20240531
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\JS68_x_JS117\fly_005
 snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\brainsss\JS68_x_JS117\fly_006
 PAUSE
