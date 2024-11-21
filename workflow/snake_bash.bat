a0fbe1e678737b8891547e2163b71b17be86eb74
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-11-21 09:51:44 -0800

    remove --unlock

--- workflow/snake_bash.bat
+++ workflow/snake_bash.bat
@@ -2,5 +2,5 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\snake_eyesss\workflow
 call activate snake_eyesss
-snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001 --unlock
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001
 PAUSE
