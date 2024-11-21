fb1cdbcb1fdc03fec118e2fee13ca6d0cdd9be25
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-11-21 08:38:16 -0800

    update paths

--- workflow/snake_bash.bat
+++ workflow/snake_bash.bat
@@ -2,7 +2,5 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\snake_eyesss\workflow
 call activate snake_eyesss
-snakemake --config user=jcsimon --profile profiles/config_Mac -s build_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\imports\20240531
-snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\JS68_x_JS117\fly_005
-snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\brainsss\JS68_x_JS117\fly_006
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001
 PAUSE
