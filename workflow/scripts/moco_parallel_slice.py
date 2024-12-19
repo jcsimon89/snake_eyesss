7aaea38564b40441ac478f106eb5e061c327791d
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2024-12-19 15:03:02 -0800

    update moco cores to 8

--- workflow/scripts/moco_parallel_slice.py
+++ workflow/scripts/moco_parallel_slice.py
@@ -655,7 +655,7 @@ if __name__ == '__main__':
     # There seems to be some multiprocessing going on in ants.registration which might explain
     # why we don't see improved times during benchmarking.
     # Hence, I fixed the core count at 8.
-    cores = 4
+    cores = 8
     if TESTING:
         cores = 4
 
