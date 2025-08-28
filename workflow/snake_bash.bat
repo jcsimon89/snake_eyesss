fae6e341e1f7e7dacba1cb9b17e943a793765ba7
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2025-08-27 18:04:10 -0700

    junk

--- workflow/snake_bash.bat
+++ workflow/snake_bash.bat
@@ -2,5 +2,12 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\snake_eyesss\workflow
 call activate snake_eyesss
-snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS015_x_JS251\fly_001
+
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_001 --unlock
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_001
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_002 --unlock
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_002
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_003 --unlock
+snakemake --config user=jcsimon --profile profiles/config_Mac -s preprocess_fly_dev.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\eyesss\JS140_x_JS259\fly_003
+
 PAUSE
