9b3da1d0b9d19b0320eaca29b928e064f5e4b426
jcsimon89 <126107847+jcsimon89@users.noreply.github.com>
2026-07-27 15:34:26 -0700

    Update snake_bash_buildfly.bat

--- workflow/snake_bash_buildfly.bat
+++ workflow/snake_bash_buildfly.bat
@@ -2,6 +2,10 @@
 ECHO Hello world
 cd C:\Users\jcsimon\Documents\GitHub\snake_eyesss\workflow
 call activate snake_eyesss
-snakemake --config user=jcsimon --profile profiles/config_Mac_buildfly -s build_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\imports\Jacob\20250823
+
+timeout /t 1 /nobreak
+
+snakemake --config user=jcsimon --profile profiles/config_Mac_buildfly -s build_fly.smk --directory C:\Users\jcsimon\Documents\Stanford\Data\Bruker\imports\Jacob\20260716
+
 
 PAUSE
